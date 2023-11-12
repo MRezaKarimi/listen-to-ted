bfad2852d7e4fa66bb493473e65214acb088218f
MR Karimi <mr.karimi77@yahoo.com>
2023-11-12 21:55:59 +0330

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import Header from "./components/Header.tsx";
 
 const App: FC = () => {
   return (
     <div
       className="h-[100vh] grid gap-x-4 main-layout"
     >
+      <Header />
     </div>
   );
 };
