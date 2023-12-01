5866aeb9258f0b2e72deefd0e2db0a0aeadd531b
MR Karimi <mr.karimi77@yahoo.com>
2023-12-01 10:47:54 +0330

    style: replace function expression with function declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import { FC } from "react";
 import Sidebar from "./components/Sidebar.tsx";
 import Content from "./components/Content.tsx";
 import Header from "./components/Header.tsx";
 
-const App: FC = () => {
+export default function App() {
   return (
-    <div
-      className="h-[100vh] grid gap-x-4 main-layout"
-    >
+    <div className="h-[100vh] grid gap-x-4 main-layout">
       <Header />
       <Sidebar />
       <Content />
     </div>
   );
-};
-
-export default App;
+}
