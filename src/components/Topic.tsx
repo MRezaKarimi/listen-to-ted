3cfe27b2cc038c2a7f101f55ebda44ed4ff713c3
MR Karimi <mr.karimi77@yahoo.com>
2023-12-01 10:35:22 +0330

    feat: add Topic component

---
+++ src/components/Topic.tsx
@@ -0,0 +1,11 @@
+function Topic() {
+  return (
+    <div
+      className="hover:bg-gray-200 text-gray-800 rounded-md px-2 py-1 cursor-pointer"
+    >
+      climate change
+    </div>
+  );
+}
+
+export default Topic;
